f05db5a23a43b83dc75c7c4a27447da901020f5f
alburquenqueletelier <alburquenque.letelier@gmail.com>
2021-10-16 22:45:51 -0300

    Se cambio planilla nueva accion y permisos para comentar(solo user identificado)

--- auctions/models.py
+++ auctions/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
 from django.forms import ModelForm, SelectDateWidget, TextInput
+from django.forms.widgets import Textarea
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import gettext_lazy as _
 
@@ -51,7 +52,7 @@ class auctionsform(ModelForm):
         exclude = ('user',)
         widgets = {
             'name': TextInput(attrs={'placeholder':'Title'}),
-            'description': TextInput(attrs={'placeholder':'Description'}),
+            'description': Textarea(attrs={'placeholder':'Description of the auction'}),
             'initial_price': TextInput(attrs={'placeholder':'Price'}),
             'date': SelectDateWidget()
         }
